5cbf522bf40c946ddd2eafbb2eec625bd47148ec
yordanos <yordanos.negash.2021@student.lhschools.org>
2020-03-06 13:39:44 -0500

    intilj commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+            System.out.println("+--------------------------");
+
+
+            System.out.println("| welcome to Gas station Brand|  ");
+            System.out.println("Los Angelas,CA ");
+            System.out.println("2378 zip cood ");
+            System.out.println("Master St ");
+            System.out.println("date 01-01-2020 ");
+            System.out.println("4:30 AM ");
+            System.out.println("Invoice 1010101010");
+            System.out.println(" AUTH");
+
+            System.out.println("Master Card Account Number ");
+            System.out.println("****************19");
+
+            System.out.println("Pump  01");
+            System.out.println("Product Unld");
+            System.out.println(" Gallons Fuel ");
+            System.out.println("20.69  ");
+            System.out.println(" Total $60.00");
+            System.out.println("Thank You ");
+            System.out.println("gas station brand ");
+
+      System.out.println("+---------------------------------");
+
+
+}
+
+
+
+
